caeb172b6f467ae8762e9dd0b6204a66b50586ff
yasserkhan45 <74680702+yasserkhan45@users.noreply.github.com>
2021-02-02 13:26:23 +0530

    Update BSearch.cpp

--- Searching/BSearch.cpp
+++ Searching/BSearch.cpp
@@ -1,28 +1,16 @@
-Q. Binary Search
+https://leetcode.com/problems/binary-search/
 
-#include <bits/stdc++.h> 
-using namespace std; 
 
-int binarySearch(int arr[], int l, int r, int x) 
-{ 
-	if (r >= l) { 
-		int mid = l + (r - l) / 2; 
-		if (arr[mid] == x) 
-			return mid; 
-		if (arr[mid] > x) 
-			return binarySearch(arr, l, mid - 1, x); 
-		return binarySearch(arr, mid + 1, r, x); 
-	} 
-	return -1; 
-} 
-
-int main(void) 
-{ 
-	int arr[] = { 2, 3, 4, 10, 40 }; 
-	int x = 10; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
-	int result = binarySearch(arr, 0, n - 1, x); 
-	(result == -1) ? cout << "Element is not present in array"
-				: cout << "Element is present at index " << result; 
-	return 0; 
-} 
+class Solution {
+  public:
+  int search(vector<int>& nums, int target) {
+    int mid, left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      mid = left + (right - left) / 2;
+      if (nums[mid] == target) return mid;
+      if (target < nums[mid]) right = mid - 1;
+      else left = mid + 1;
+    }
+    return -1;
+  }
+};
